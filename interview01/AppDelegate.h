e16110ec508649a1a2898bef5d2081fd83294764
hanghang <tangge@hanghangdeMacBook-Pro.local>
2021-02-23 20:58:39 +0800

    初始化

---
+++ interview01/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  interview01
+//
+//  Created by hanghang on 2021/1/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ interview01/ViewController.m
@@ -0,0 +1,89 @@
+//
+//  ViewController.m
+//  interview01
+//
+//  Created by hanghang on 2021/1/19.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+//两个大数相加算法
+-(NSString *)addTwoNumberWithOneNumStr:(NSString *)one anotherNumStr:(NSString *)another
+{
+    int i = 0;
+    int j = 0;
+    int maxLength = 0;
+    int sum = 0;
+    int overflow = 0;
+    int carryBit = 0;
+    NSString *temp1 = @"";
+    NSString *temp2 = @"";
+    NSString *sums = @"";
+    NSString *tempSum = @"";
+    int length1 = (int)one.length;
+    int length2 = (int)another.length;
+    //1.反转字符串
+    for (i = length1 - 1; i >= 0 ; i--) {
+        NSRange range = NSMakeRange(i, 1);
+        temp1 = [temp1 stringByAppendingString:[one substringWithRange:range]];
+        NSLog(@"%@",temp1);
+    }
+    for (j = length2 - 1; j >= 0; j--) {
+        NSRange range = NSMakeRange(j, 1);
+        temp2 = [temp2 stringByAppendingString:[another substringWithRange:range]];
+        NSLog(@"%@",temp2);
+    }
+    
+    //2.补全缺少位数为0
+    maxLength = length1 > length2 ? length1 : length2;
+    if (maxLength == length1) {
+        for (i = length2; i < length1; i++) {
+            temp2 = [temp2 stringByAppendingString:@"0"];
+            NSLog(@"i = %d --%@",i,temp2);
+        }
+    }else{
+        for (j = length1; j < length2; j++) {
+            temp1 = [temp1 stringByAppendingString:@"0"];
+            NSLog(@"j = %d --%@",j,temp1);
+        }
+    }
+    //3.取数做加法
+    for (i = 0; i < maxLength; i++) {
+        NSRange range = NSMakeRange(i, 1);
+        int a = [temp1 substringWithRange:range].intValue;
+        int b = [temp2 substringWithRange:range].intValue;
+        sum = a + b + carryBit;
+        if (sum > 9) {
+            if (i == maxLength -1) {
+                overflow = 1;
+            }
+            carryBit = 1;
+            sum -= 10;
+        }else{
+            carryBit = 0;
+        }
+        tempSum = [tempSum stringByAppendingString:[NSString stringWithFormat:@"%d",sum]];
+    }
+    if (overflow == 1) {
+        tempSum = [tempSum stringByAppendingString:@"1"];
+    }
+    int sumlength = (int)tempSum.length;
+    for (i = sumlength - 1; i >= 0 ; i--) {
+        NSRange range = NSMakeRange(i, 1);
+        sums = [sums stringByAppendingString:[tempSum substringWithRange:range]];
+    }
+    NSLog(@"sums = %@",sums);
+    return sums;
+}
+
+@end
